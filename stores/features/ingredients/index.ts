c7a767ef88a6594b8806893c8a03f696dc648e1a
Magali MAI <maimagali26.pro@gmail.com>
2026-03-04 12:44:37 +0100

    Update ingrdientsSlice initial state structure

--- stores/features/ingredients/index.ts
+++ stores/features/ingredients/index.ts
@@ -1,16 +1,35 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export interface Ingredient {}
+export interface Ingredient {
+  id: string;
+  name: string;
+  category: string;
+  stockQuantity: number;
+  unit: string;
+  menuCategories: string[];
+}
+
+type SelectedId = null | string;
 
 const initialState = {
-  ingredients: [] as Ingredient[],
+  ingredients: {
+    list: [] as Ingredient[],
+    selectedId: null as SelectedId,
+  },
 };
 
 export const ingredientSlice = createSlice({
   name: "ingredients",
   initialState,
-  reducers: {},
+  reducers: {
+    ingredientsUpdated: (state, action: PayloadAction<Ingredient[]>) => {
+      state.ingredients.list = action.payload;
+    },
+    selectIngredient: (state, action: PayloadAction<SelectedId>) => {
+      state.ingredients.selectedId = action.payload;
+    }
+  },
 });
 
-export const {} = ingredientSlice.actions;
+export const { ingredientsUpdated } = ingredientSlice.actions;
 export default ingredientSlice.reducer;
--- stores/thunks/ingredients/index.ts
+++ stores/thunks/ingredients/index.ts
@@ -4,7 +4,9 @@ async function setQuantifiable() {}
 
 async function updateIngredient() {}
 
-async function selectIngredient() {}
+async function selectIngredient() {
+  // Mett
+}
 
 async function updateStockFromShopping() {}
 
@@ -14,4 +16,22 @@ async function addIngredient() {}
 
 async function updateStockFromMenu() {}
 
-async function fetchIngredients() {}
+async function fetchIngredients() {
+/*
+SELECT
+  i.id_ingredients,
+  GROUP_CONCAT(DISTINCT mc.name ORDER BY mc.name) AS menu_categories,
+  ic.name AS category_name,
+  i.name AS ingredient_name,
+  i.stock_quantity,
+  u.abbreviation AS abbreviation
+FROM
+  ingredients i
+  JOIN units u ON u.id_units = i.id_units
+  JOIN ingredient_categories ic ON ic.id_ingredient_categories = i.id_ingredient_categories
+  JOIN ingredient_menu_category_links imc ON imc.id_ingredients = i.id_ingredients
+  JOIN menu_categories mc ON mc.id_menu_categories = imc.id_menu_categories
+GROUP BY i.id_ingredients;
+*/
+}
+
