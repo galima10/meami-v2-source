3a079bc0f17305b3a09ae27a0585ed18d0e00a56
Magali MAI <maimagali26.pro@gmail.com>
2026-03-09 10:40:06 +0100

    Configure actions slice and add SQL request in comments into thunks for manual adjustements slice

--- stores/features/manualAdjustements/index.ts
+++ stores/features/manualAdjustements/index.ts
@@ -1,16 +1,50 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { act } from "react";
 
-export interface ManualAdjustement {}
+export interface ManualAdjustementItem {
+  id: string;
+  name: string;
+  unit: string;
+  usageCount: number;
+  checked: boolean;
+}
 
 const initialState = {
-  manualAdjustements: [] as ManualAdjustement[],
+  shopping: [] as ManualAdjustementItem[],
+  stock: [] as ManualAdjustementItem[],
 };
 
 export const manualAdjustementSlice = createSlice({
   name: "manualAdjustements",
   initialState,
-  reducers: {},
+  reducers: {
+    setManualAdjustements: (
+      state,
+      action: PayloadAction<{
+        ingredients: ManualAdjustementItem[];
+        type: "shopping" | "stock";
+      }>,
+    ) => {
+      const { ingredients, type } = action.payload;
+      state[type] = ingredients;
+    },
+    ingredientCheckToggled: (
+      state,
+      action: PayloadAction<{
+        type: "shopping" | "stock";
+        ingredientId: string;
+        checked: boolean;
+      }>,
+    ) => {
+      const { type, ingredientId, checked } = action.payload;
+      const index = state[type].findIndex((item) => item.id === ingredientId);
+
+      if (index !== -1) {
+        state[type][index].checked = checked;
+      }
+    },
+  },
 });
 
-export const {} = manualAdjustementSlice.actions;
+export const { setManualAdjustements, ingredientCheckToggled } = manualAdjustementSlice.actions;
 export default manualAdjustementSlice.reducer;
--- stores/thunks/manualAdjustements/index.ts
+++ stores/thunks/manualAdjustements/index.ts
@@ -1,7 +1,151 @@
-async function loadManualShoppingChecks() {}
+import {
+  setManualAdjustements,
+  ingredientCheckToggled,
+  ManualAdjustementItem,
+} from "@stores/features/manualAdjustements";
 
-async function loadManualStockChecks() {}
+async function loadManualChecks(type: "shopping" | "stock") {
+  /*
 
-async function removeManualChecks() {}
 
-async function setIngredientChecked() {}
+=> Si type === "shoppoing" :
+
+INSERT INTO
+  shopping_list_manual_checks (
+    id_shopping_lists,
+    id_ingredients,
+    usage_count
+  )
+SELECT
+  sl.id_shopping_lists,
+  i.id_ingredients,
+  COUNT(mil.id_ingredients) AS usage_count
+FROM
+  shopping_lists sl
+  JOIN menus m ON 1 = 1
+  JOIN menu_ingredient_links mil ON mil.id_menus = m.id_menus
+  JOIN ingredients i ON i.id_ingredients = mil.id_ingredients
+  JOIN units u ON u.id_units = i.id_units
+WHERE
+  i.quantifiable = FALSE
+  AND u.name <> "Infini"
+  AND NOT EXISTS (
+    SELECT
+      1
+    FROM
+      shopping_list_manual_checks slmc
+    WHERE
+      slmc.id_shopping_lists = sl.id_shopping_lists
+      AND slmc.id_ingredients = i.id_ingredients
+  )
+GROUP BY
+  sl.id_shopping_lists,
+  i.id_ingredients;
+
+
+
+SELECT
+  i.id_ingredients,
+  i.name AS ingredient_name,
+  u.abbreviation AS unit,
+  slmc.usage_count,
+  slmc.checked
+FROM
+  shopping_list_manual_checks slmc
+  JOIN ingredients i ON i.id_ingredients = slmc.id_ingredients
+  JOIN units u ON u.id_units = i.id_units;
+
+
+=> Si type === "stock"
+
+INSERT INTO
+  menu_ingredient_manual_checks (id_ingredients, usage_count)
+SELECT
+  i.id_ingredients,
+  COUNT(mil.id_menus) AS usage_count
+FROM
+  ingredients i
+  JOIN menu_ingredient_links mil ON mil.id_ingredients = i.id_ingredients
+  JOIN units u ON u.id_units = i.id_units
+WHERE
+  i.quantifiable = FALSE
+  AND u.name <> "Infini"
+GROUP BY
+  i.id_ingredients
+ORDER BY
+  usage_count DESC;
+
+
+
+SELECT
+  i.id_ingredients,
+  i.name AS ingredient_name,
+  u.abbreviation AS unit,
+  mimc.usage_count,
+  mimc.checked
+FROM
+  menu_ingredient_manual_checks mimc
+  JOIN ingredients i ON i.id_ingredients = mimc.id_ingredients
+  JOIN units u ON u.id_units = i.id_units;
+
+
+*/
+  // dispatch manualAdjustementsSlice setManualAdjustements ingredients type
+}
+
+async function removeManualChecks(type: "shopping" | "stock") {
+  /*
+
+let tableCible;
+
+=> Si type === "shopping" :
+
+tableCible = "shopping_list";
+
+=> Si type === "stock" :
+
+tableCible = "menu_ingredient";
+
+
+
+DELETE FROM
+  tableCible_manual_checks;
+
+
+*/
+  // dispatch manualAdjustementsSlice setManualAdjustements [] type
+}
+
+async function setIngredientCheck(
+  type: "shopping" | "stock",
+  ingredientId: string,
+  checked: boolean,
+) {
+/*
+
+
+let tableCible;
+
+=> Si type === "shopping" :
+
+tableCible = "shopping_list";
+
+=> Si type === "stock" :
+
+tableCible = "menu_ingredient";
+
+
+
+UPDATE
+  tableCible tcmc
+  JOIN ingredients i ON i.id_ingredients = tcmc.id_ingredients
+  JOIN units u ON u.id_units = i.id_units
+SET
+  tcmc.checked = checked
+WHERE
+  i.id_ingredients = ingredientId;
+
+
+*/
+// dispatch manualAdjustementsSlice ingredientCheckToggled type ingredientId checked
+}
