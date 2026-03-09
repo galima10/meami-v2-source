fee61db0db0414933ff466851cde5c28b00f33af
Magali MAI <maimagali26.pro@gmail.com>
2026-03-09 11:03:01 +0100

    Configure actions slice and add SQL request in comments into thunks for cooking ustensils slice

--- stores/features/cookingUstensils/index.ts
+++ stores/features/cookingUstensils/index.ts
@@ -1,6 +1,9 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export interface CookingUstensil {}
+export interface CookingUstensil {
+  id: string;
+  name: string;
+}
 
 const initialState = {
   cookingUstensils: [] as CookingUstensil[],
@@ -9,8 +12,25 @@ const initialState = {
 export const cookingUstensilSlice = createSlice({
   name: "cookingUstensils",
   initialState,
-  reducers: {},
+  reducers: {
+    setCookingUstensils: (state, action: PayloadAction<CookingUstensil[]>) => {
+      state.cookingUstensils = action.payload;
+    },
+    cookingUstensilAdded: (state, action: PayloadAction<CookingUstensil>) => {
+      state.cookingUstensils.push(action.payload);
+    },
+    cookingUstensilDeleted: (state, action: PayloadAction<string>) => {
+      const cookingUstensilId = action.payload;
+      state.cookingUstensils = state.cookingUstensils.filter(
+        (item) => item.id !== cookingUstensilId,
+      );
+    },
+  },
 });
 
-export const {} = cookingUstensilSlice.actions;
+export const {
+  setCookingUstensils,
+  cookingUstensilAdded,
+  cookingUstensilDeleted,
+} = cookingUstensilSlice.actions;
 export default cookingUstensilSlice.reducer;
--- stores/thunks/cookingUstensils/index.ts
+++ stores/thunks/cookingUstensils/index.ts
@@ -1,5 +1,49 @@
-async function createUstensil() {}
+import {
+  setCookingUstensils,
+  cookingUstensilAdded,
+  cookingUstensilDeleted,
+  CookingUstensil,
+} from "@stores/features/cookingUstensils";
 
-async function deleteUstensil() {}
+async function createUstensil(newCookingUstensil: CookingUstensil) {
+/*
 
-async function fetchCookingUstensils() {}
+
+INSERT INTO
+  cooking_ustensils (name)
+VALUES
+  (newCookingUstensil.name);
+
+
+*/
+// dispatch cookingUstensilAdded newCookingUstensil
+}
+
+async function deleteUstensil(cookingUstensilId: string) {
+/*
+
+
+DELETE cu
+FROM
+  cooking_ustensils cu
+WHERE
+  cu.id_cooking_ustensils = cookingUstensilId;
+
+
+*/
+// dispatch cookingUstensilDeleted cookingUstensilId
+}
+
+async function fetchCookingUstensils() {
+  /*
+
+
+SELECT
+  *
+FROM
+  cooking_ustensils;
+
+
+*/
+  // dispatch cookingUstensilsSlice setCookingUstensils
+}
