2cecb96a0362110a2cde83e412e8a64fd8ff0522
Magali MAI <maimagali26.pro@gmail.com>
2026-03-13 00:19:31 +0100

    Prepare seeds types and transactions for futures insert

--- database/init.ts
+++ database/init.ts
@@ -1,33 +1,49 @@
-import { getDb } from "./database";
 import * as SQLite from "expo-sqlite";
 
-// Création des tables
 export async function initDatabase(db: SQLite.SQLiteDatabase) {
-  await db.execAsync(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      email TEXT
-    );
-
-    CREATE TABLE IF NOT EXISTS posts (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT,
-      user_id INTEGER
-    );
-  `);
+  await db.withExclusiveTransactionAsync(async () => {
+    await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        email TEXT
+      );
+      CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        user_id INTEGER
+      );
+    `);
+  });
 }
 
+interface Seeds {
+  name: string;
+  email: string;
+}
+
+const seeds: Seeds[] = [
+  {
+    name: "",
+    email: "",
+  },
+];
+
 export async function seedDatabase(db: SQLite.SQLiteDatabase) {
+  // Une table = une transaction
   const users = await db.getAllAsync("SELECT * FROM users");
   if (users.length === 0) {
-    await db.runAsync(
-      "INSERT INTO users (name, email) VALUES (?, ?)",
-      ["Alice", "alice@example.com"]
-    );
-    await db.runAsync(
-      "INSERT INTO users (name, email) VALUES (?, ?)",
-      ["Bob", "bob@example.com"]
-    );
+    await db.withExclusiveTransactionAsync(async () => {
+      const stmt = await db.prepareAsync(
+        "INSERT INTO users (name, email) VALUES (?, ?)",
+      );
+      try {
+        for (const u of seeds) {
+          await stmt.executeAsync([u.name, u.email]);
+        }
+      } finally {
+        await stmt.finalizeAsync();
+      }
+    });
   }
 }
