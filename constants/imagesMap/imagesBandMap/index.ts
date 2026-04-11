56df86e6b112093aa865954415f64862bce86942
Magali MAI <maimagali26.pro@gmail.com>
2026-04-11 22:41:45 +0200

    Add images map for requires images

---
+++ constants/imagesMap/imagesBandMap/index.ts
@@ -0,0 +1,29 @@
+export const imagesMap: Record<string, any> = {
+  monday_morning: require("@assets/images/requires/bands/menu_monday_morning_3x.jpg"),
+  monday_noon: require("@assets/images/requires/bands/menu_monday_noon_3x.jpg"),
+  monday_evening: require("@assets/images/requires/bands/menu_monday_evening_3x.jpg"),
+
+  tuesday_morning: require("@assets/images/requires/bands/menu_tuesday_morning_3x.jpg"),
+  tuesday_noon: require("@assets/images/requires/bands/menu_tuesday_noon_3x.jpg"),
+  tuesday_evening: require("@assets/images/requires/bands/menu_tuesday_evening_3x.jpg"),
+
+  wednesday_morning: require("@assets/images/requires/bands/menu_wednesday_morning_3x.jpg"),
+  wednesday_noon: require("@assets/images/requires/bands/menu_wednesday_noon_3x.jpg"),
+  wednesday_evening: require("@assets/images/requires/bands/menu_wednesday_evening_3x.jpg"),
+
+  thursday_morning: require("@assets/images/requires/bands/menu_thursday_morning_3x.jpg"),
+  thursday_noon: require("@assets/images/requires/bands/menu_thursday_noon_3x.jpg"),
+  thursday_evening: require("@assets/images/requires/bands/menu_thursday_evening_3x.jpg"),
+
+  friday_morning: require("@assets/images/requires/bands/menu_friday_morning_3x.jpg"),
+  friday_noon: require("@assets/images/requires/bands/menu_friday_noon_3x.jpg"),
+  friday_evening: require("@assets/images/requires/bands/menu_friday_evening_3x.jpg"),
+
+  saturday_morning: require("@assets/images/requires/bands/menu_saturday_morning_3x.jpg"),
+  saturday_noon: require("@assets/images/requires/bands/menu_saturday_noon_3x.jpg"),
+  saturday_evening: require("@assets/images/requires/bands/menu_saturday_evening_3x.jpg"),
+
+  sunday_morning: require("@assets/images/requires/bands/menu_sunday_morning_3x.jpg"),
+  sunday_noon: require("@assets/images/requires/bands/menu_sunday_noon_3x.jpg"),
+  sunday_evening: require("@assets/images/requires/bands/menu_sunday_evening_3x.jpg"),
+};
