124cb2c74196ab5e28fb1235edd8fe256fe31881
Magali MAI <maimagali26.pro@gmail.com>
2026-04-13 08:46:48 +0200

    Add useEffect redirection to calendar screen for splash screen

--- app/(splash)/index.tsx
+++ app/(splash)/index.tsx
@@ -3,17 +3,20 @@ import { useRouter } from "expo-router";
 import { AppText } from "@modules/shared/components/atoms/AppText";
 import theme from "@constants/themes";
 import { typography } from "@constants/styles";
+import { useEffect } from "react";
 
 export default function Splash() {
   const router = useRouter();
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.replace("/(tabs)/menuTab/MenuCalendarScreen");
+  //   }, 5000);
+
+  //   return () => clearTimeout(timer);
+  // }, []);
   return (
     <View style={styles.container}>
       <AppText style={styles.text}>Splash Screen</AppText>
-      <Pressable
-        onPress={() => router.replace("/(tabs)/menuTab/MenuCalendarScreen")}
-      >
-        <AppText style={styles.button}>Sortir du splash screen</AppText>
-      </Pressable>
     </View>
   );
 }
