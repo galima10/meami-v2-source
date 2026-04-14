de4cd168f42bd765b2b4d4e9c55f4bd5f61dbad0
Magali MAI <maimagali26.pro@gmail.com>
2026-04-14 13:58:28 +0200

    Import a component from v1 : WaveText with its hook

---
+++ modules/shared/components/atoms/animated/WaveText/index.tsx
@@ -0,0 +1,54 @@
+import { AnimatedAppText } from "../../appElements/AppText";
+import { useWaveText } from "@modules/shared/hooks/atoms/animated/useWaveText";
+import React from "react";
+import {
+  StyleProp,
+  StyleSheet,
+  TextProps,
+  TextStyle,
+  View,
+} from "react-native";
+
+type WaveTextProps = TextProps & {
+  text: string;
+  style?: StyleProp<TextStyle>;
+};
+
+export default function WaveText({ text, style, ...rest }: WaveTextProps) {
+  const words = text.split(/(\s+)/);
+
+  return (
+    <View style={styles.container}>
+      {words.map((word, wordIndex) => {
+        return (
+          <View key={wordIndex} style={styles.wordContainer}>
+            {word.split("").map((char, charIndex) => {
+              const animatedStyle = useWaveText(charIndex + wordIndex * 3);
+              return (
+                <AnimatedAppText
+                  key={charIndex}
+                  {...rest}
+                  style={[style, animatedStyle]}
+                >
+                  {char}
+                </AnimatedAppText>
+              );
+            })}
+          </View>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  wordContainer: {
+    flexDirection: "row",
+    marginVertical: 16,
+  },
+});
---
+++ modules/shared/hooks/atoms/animated/useWaveText/index.ts
@@ -0,0 +1,31 @@
+import { useSharedValue, useAnimatedStyle, withRepeat, withTiming, Easing } from "react-native-reanimated";
+import { useEffect } from "react";
+
+export const useWaveText = (index:number, { amplitude = 10, speed = 2800 } = {}) => {
+  const progress = useSharedValue(0);
+  const waveLength = 5;
+
+  useEffect(() => {
+    progress.value = withRepeat(
+      withTiming(2 * Math.PI, {
+        duration: speed,
+        easing: Easing.linear,
+      }),
+      -1,
+      false
+    );
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const offset = index / waveLength;
+    return {
+      transform: [
+        {
+          translateY: Math.sin(progress.value + offset) * amplitude,
+        },
+      ],
+    };
+  });
+
+  return animatedStyle;
+};
