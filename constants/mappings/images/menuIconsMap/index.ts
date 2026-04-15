11f0f154029d63916d858d54fc3ca0bf83772251
Magali MAI <maimagali26.pro@gmail.com>
2026-04-15 13:31:30 +0200

    Fix errors name into menuIconsMap

---
+++ constants/mappings/images/menuIconsMap/index.ts
@@ -0,0 +1,15 @@
+export const menuIconsMap: Record<string, any> = {
+  monday_icons: require("@assets/images/requires/icons/monday_icons_3x.png"),
+
+  tuesday_icons: require("@assets/images/requires/icons/tuesday_icons_3x.png"),
+
+  wednesday_icons: require("@assets/images/requires/icons/wednesday_icons_3x.png"),
+
+  thursday_icons: require("@assets/images/requires/icons/thursday_icons_3x.png"),
+
+  friday_icons: require("@assets/images/requires/icons/friday_icons_3x.png"),
+
+  saturday_icons: require("@assets/images/requires/icons/saturday_icons_3x.png"),
+
+  sunday_icons: require("@assets/images/requires/icons/sunday_icons_3x.png"),
+};
--- modules/menuTab/components/molecules/MenuContainer/index.tsx
+++ modules/menuTab/components/molecules/MenuContainer/index.tsx
@@ -1,9 +1,17 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import theme from "@constants/themes";
+import { dayColors } from "@constants/mappings/colors/dayColors";
+import { menuIconsMap } from "@constants/mappings/images/menuIconsMap";
 
 export default function MenuContainer() {
-  return <View style={styles.container}></View>;
+  return (
+    <ImageBackground
+      resizeMode="contain"
+      source={menuIconsMap.monday_icons}
+      style={styles.container}
+    ></ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
