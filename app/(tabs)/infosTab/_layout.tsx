83e730af35c0e0d3ffec7907fed5646d6a4cecbd
Magali MAI <maimagali26.pro@gmail.com>
2026-04-15 12:29:47 +0200

    Add top bar for infos tab

--- app/(tabs)/infosTab/_layout.tsx
+++ app/(tabs)/infosTab/_layout.tsx
@@ -1,5 +1,41 @@
 import { Stack } from "expo-router";
+import { ROUTES } from "@constants/general";
+import AppTopBar from "@modules/shared/components/molecules/AppTopBar";
+import TopButton from "@modules/shared/components/atoms/buttons/TopButton";
+import { usePathname } from "expo-router";
 
 export default function InfosTabLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const pathname = usePathname();
+  const INFOS_TAB = ROUTES.infos;
+  function infosAddRoute() {
+    if (pathname === INFOS_TAB.cookingList) return INFOS_TAB.cookingForm;
+    else if (pathname === INFOS_TAB.storageList) return INFOS_TAB.storageForm;
+  }
+  return (
+    <Stack
+      screenOptions={{
+        header: () => (
+          <AppTopBar
+            left={
+              <>
+                <TopButton icon="cookingIcon" route={INFOS_TAB.cookingList} />
+                <TopButton icon="closetIcon" route={INFOS_TAB.storageList} />
+              </>
+            }
+            right={
+              <>
+                {pathname !== INFOS_TAB.default && (
+                  <TopButton
+                    icon="addIcon"
+                    color="green"
+                    route={infosAddRoute()}
+                  />
+                )}
+              </>
+            }
+          />
+        ),
+      }}
+    />
+  );
 }
