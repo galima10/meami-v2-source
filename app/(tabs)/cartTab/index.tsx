24c40f65ff89edf0a9049bc01b53fdf048b907ca
Magali MAI <maimagali26.pro@gmail.com>
2026-04-15 12:06:56 +0200

    Add navigation to cart list screen

--- app/(tabs)/cartTab/index.tsx
+++ app/(tabs)/cartTab/index.tsx
@@ -1,12 +1,18 @@
 import { typography } from "@constants/styles";
 import theme from "@constants/themes";
 import { AppText } from "@modules/shared/components/primitives/AppText";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Pressable } from "react-native";
+import { useAppNavigation } from "@modules/shared/hooks/useAppNavigation";
+import { ROUTES } from "@constants/general";
 
 export default function CartDefaultScreen() {
+  const { handleNavigate } = useAppNavigation();
   return (
     <View style={styles.container}>
       <AppText style={styles.text}>Cart Default Screen</AppText>
+      <Pressable onPress={() => handleNavigate(ROUTES.cart.list)}>
+        <AppText>Générer liste de courses</AppText>
+      </Pressable>
     </View>
   );
 }
