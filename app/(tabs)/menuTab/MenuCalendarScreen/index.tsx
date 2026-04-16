e2bcaf068acb780d06cbea6fa23996235f9bddd2
Magali MAI <maimagali26.pro@gmail.com>
2026-04-16 18:27:52 +0200

    Mapping days into DayCardCalendar

--- app/(tabs)/menuTab/MenuCalendarScreen/index.tsx
+++ app/(tabs)/menuTab/MenuCalendarScreen/index.tsx
@@ -2,8 +2,32 @@ import { typography } from "@constants/styles";
 import theme from "@constants/themes";
 import { StyleSheet, View, ScrollView } from "react-native";
 import DayCardCalendar from "@modules/menuTab/components/organisms/DayCardCalendar";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@modules/shared/hooks/redux";
+import {
+  fetchAllMenusThunk,
+  fetchWeeklyMenuThunk,
+} from "@stores/thunks/weeklyMenu";
+import {
+  weeklyMenuToUi,
+  MenuUi,
+  MomentUi,
+} from "@utils/dataToUi/weeklyMenuToUi";
 
 export default function MenuCalendarScreen() {
+  const dispatch = useAppDispatch();
+  const { weeklyMenu } = useAppSelector((state) => state.weeklyMenu);
+  const { moments, days, menuCategories } = useAppSelector(
+    (state) => state.seed,
+  );
+  const weeklyMenuUi = weeklyMenuToUi(weeklyMenu, days, moments);
+  useEffect(() => {
+    async function fetchMenus() {
+      await dispatch(fetchAllMenusThunk());
+      await dispatch(fetchWeeklyMenuThunk());
+    }
+    fetchMenus();
+  }, []);
   return (
     <View style={styles.container}>
       <ScrollView
@@ -11,8 +35,13 @@ export default function MenuCalendarScreen() {
         showsHorizontalScrollIndicator={false}
         pagingEnabled
       >
-        <DayCardCalendar moment="matin" day="samedi" />
-        <DayCardCalendar moment="matin" day="dimanche" />
+        {/* <DayCardCalendar moment="matin" day="samedi" />
+        <DayCardCalendar moment="matin" day="dimanche" /> */}
+        {(Object.keys(weeklyMenuUi) as string[]).map((day) => {
+          return (
+            <DayCardCalendar key={day} moment="matin" day={day.toLowerCase()} />
+          );
+        })}
       </ScrollView>
     </View>
   );
