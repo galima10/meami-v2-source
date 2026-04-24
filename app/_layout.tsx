79af2d884f4c849fb63253cc220b6afc0df1fe0f
Magali MAI <maimagali26.pro@gmail.com>
2026-04-24 16:16:48 +0200

    Rectifie initial fetch data

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,28 @@
 import { getDb } from "@database/database";
 import { store } from "@stores/index";
 import { fetchInitialDataThunk } from "@stores/thunks/seeds";
-import { fetchAllMenusThunk } from "@stores/thunks/weeklyMenu";
 import { Stack } from "expo-router";
-import { useAppDispatch } from "modules/shared/hooks/redux";
+import { useAppDispatch, useAppSelector } from "modules/shared/hooks/redux";
 import { useEffect } from "react";
 import { Provider } from "react-redux";
 import HiddenResetButton from "@modules/shared/components/atoms/test/HiddenResetButton";
 import TestButton from "@modules/shared/components/atoms/test/TestButton";
+import type { AppDispatch } from "@stores/index";
+
+async function initDBAndFetch(dispatch: AppDispatch) {
+  await getDb();
+  dispatch(fetchInitialDataThunk());
+}
 
 function InitData() {
   const dispatch = useAppDispatch();
+  const seedsLoaded = useAppSelector((state) => state.seed.hasLoaded);
 
   useEffect(() => {
-    async function initDBAndFetch() {
-      await getDb();
-      dispatch(fetchInitialDataThunk());
-      
+    if (!seedsLoaded) {
+      initDBAndFetch(dispatch);
     }
-
-    initDBAndFetch();
-  }, []);
+  }, [seedsLoaded]);
 
   return null;
 }
